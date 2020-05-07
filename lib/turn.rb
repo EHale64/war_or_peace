961896f61f4c8f9b9e5541ed573bb6215a064c09
Eric Hale <eric_hale@comcast.net>
2020-05-07 14:31:43 -0600

    Add method and test for war winner

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,6 +24,12 @@ class Turn
       elsif player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
         player2
       end
+    elsif type == :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        player1
+      elsif player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+        player2
+      end
     end
   end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -61,6 +61,26 @@ class PlayerTest < Minitest::Test
   end
 
   def test_it_can_find_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    deck2 = Deck.new([@card4, @card3, @card6, @card7])
+    deck3 = Deck.new([card1, card2, card5, card8])
+    deck4 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    player3 = Player.new("Megan", deck3)
+    player4 = Player.new("Aurora", deck4)
+    turn2 = Turn.new(player1, player2)
+    turn3 = Turn.new(player3, player4)
     assert_equal @player1, @turn.winner
+    assert_equal player2, turn2.winner
+    #assert_equal @player1, @turn.winner
   end
 end
